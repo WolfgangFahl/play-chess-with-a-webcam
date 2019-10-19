273a2455b9fb9fc910f71edc057c6b9000262c79
Wolfgang Fahl <wf@bitplan.com>
2019-10-19 10:11:28 +0200

    fixes key handling

--- opencvtutorial/histogramm.py
+++ opencvtutorial/histogramm.py
@@ -18,17 +18,18 @@ def main(argv):
     filename = argv[0] if len(argv) > 0 else default_file
     image = cv2.imread(filename)
     cv2.imshow('chessboard',image)
+    # refresh
+    cv2.waitKey(10)
     hsv=cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     plt.figure(figsize=(10,8))
+    plt.suptitle('HSV Histogramm', fontsize=16)
     plotChannel(hsv,0,3,1,'Hue')
     plt.subplots_adjust(hspace=.5)
     plotChannel(hsv,1,3,2,'Saturation')
     plotChannel(hsv,2,3,3,'Luminosity Value')
+    fig=plt.figure(1)
+    fig.canvas.set_window_title('Histogramm for %s' % (filename))
     plt.show()
-
-    while True:
-        k = cv2.waitKey(0) & 0xFF
-        if k == 27: break             # ESC key to exit
     cv2.destroyAllWindows()
     return 0
 
