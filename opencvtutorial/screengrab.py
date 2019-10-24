d3ce8d9866287163ccb4c73a263517764336d164
Wolfgang Fahl <wf@neso.bitplan.com>
2019-10-24 22:53:11 +0200

    adds screen scraping examples

---
+++ opencvtutorial/screengrab.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+# part of https://github.com/WolfgangFahl/play-chess-with-a-webcam
+
+# see https://stackoverflow.com/a/54246290/1497139
+# renders at > 65 fps on a mac mini
+import numpy as np
+import cv2
+from mss import mss
+from PIL import Image
+import time
+
+last_time = time.time()
+bbox = {'top': 50, 'left': 50, 'width': 800, 'height': 640}
+sum=0
+count=0
+sct = mss()
+while 1:
+    sct_img = sct.grab(bbox)
+    grabtime=time.time()-last_time
+    sum+=1/grabtime
+    count=count+1
+    print('grab took %3.2f s= %4.1f fps avg=%4.1f fps' % (grabtime,1/grabtime,sum/count))
+    cv2.imshow('screen', np.array(sct_img))
+    last_time = time.time()
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
---
+++ opencvtutorial/screengrab2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+# part of https://github.com/WolfgangFahl/play-chess-with-a-webcam
+# https://stackoverflow.com/a/51643195/1497139
+
+import numpy as np
+import cv2
+from PIL import ImageGrab as ig
+import time
+
+last_time = time.time()
+while(True):
+    bbox=(50,50,800,640)
+    screen = ig.grab(bbox=bbox)
+    grabtime=time.time()-last_time
+    print('grab took %3.1f s= %3.1f fps' % (grabtime,1/grabtime))
+    title="screen %d,%d %d,%d " % (bbox)
+    cv2.imshow(title, np.array(screen))
+    last_time = time.time()
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
