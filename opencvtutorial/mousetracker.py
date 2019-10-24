b17357c89e159052cb611040cc7b66700c3a4448
Wolfgang Fahl <wf@neso.bitplan.com>
2019-10-24 23:50:13 +0200

    adds mousetracker example

---
+++ opencvtutorial/mousetracker.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+# part of https://github.com/WolfgangFahl/play-chess-with-a-webcam
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.core.window import Window
+from kivy.uix.gridlayout import GridLayout
+
+class TrackerScreen(GridLayout):
+    def __init__(self, **kwargs):
+        super(TrackerScreen, self).__init__(**kwargs)
+        self.cols = 2
+        self.mouseLabel=self.addLabel("MousePos:")
+        Window.bind(mouse_pos=lambda w, p: setattr(self.mouseLabel, 'text', str(p)+"("+str(Window.left)+","+str(Window.top)+")"+str(Window.size)))
+
+    def addLabel(self,title):
+        newLabel = Label()
+        self.add_widget(Label(text=title))
+        self.add_widget(newLabel)
+        return newLabel
+
+
+class MousePos(App):
+    def build(self):
+        return TrackerScreen()
+
+if __name__ == '__main__':
+    MousePos().run()
