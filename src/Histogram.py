bf04aeda529eeca0fe244b51fb77aa600674b06c
Wolfgang Fahl <wf@bitplan.com>
2019-10-31 22:14:37 +0100

    adds more test warpPoints

--- src/Histogram.py
+++ src/Histogram.py
@@ -22,19 +22,25 @@ class Histogram:
         cImg = img[:, :, channel]
         plt.subplot(rows, col, plotNum)
         plt.hist(np.ndarray.flatten(cImg), bins=256)
-        plt.title(title)    
+        plt.title(title)
+        
+    def plot3Channels(self,img,rows,plotNum,c1title,c2title,c3title):        
+        self.plotChannel(img, 0, 1, rows, plotNum+1, c1title)
+        plt.subplots_adjust(hspace=.7)
+        self.plotChannel(img, 1, 1, rows, plotNum+2, c2title)
+        self.plotChannel(img, 2, 1, rows, plotNum+3, c3title)
                 
-    def show(self,waitTime):
+    def show(self,waitTime,close=False):
         hsv = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
         plt.figure(figsize=(10, 8))
-        plt.suptitle('HSV Histogramm', fontsize=16)
-        self.plotChannel(hsv, 0, 1, 4, 2, 'Hue')
-        plt.subplots_adjust(hspace=.5)
-        self.plotChannel(hsv, 1, 1, 4, 3, 'Saturation')
-        self.plotChannel(hsv, 2, 1, 4, 4, 'Luminosity Value')
+        localTitle='Histogram for %s' % (self.title)
+        plt.suptitle(localTitle, fontsize=16)
+        self.plot3Channels(hsv,6,0,'Hue','Saturation','Luminosity Value')
+        self.plot3Channels(self.image,6,3,'Blue','Green','Red')
         fig = plt.figure(1)
-        fig.canvas.set_window_title('Histogramm for %s' % (self.title))
+        fig.canvas.set_window_title(localTitle)
         plt.show(block=False)
+        plt.savefig('/tmp/'+self.title)
         if Histogram.debug:
             self.video.showImage(self.image,self.title,keyCheck=True,keyWait=waitTime)
         self.video.close()
