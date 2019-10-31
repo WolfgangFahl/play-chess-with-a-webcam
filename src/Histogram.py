f92109abceba043ca804ee015176188f1e7ecca8
Wolfgang Fahl <wf@bitplan.com>
2019-10-31 21:45:20 +0100

    adds histogram examples

--- src/Histogram.py
+++ src/Histogram.py
@@ -10,6 +10,32 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 class Histogram:
+    debug=True
+    
     # construct me 
-    def __init__(self):
-        pass
+    def __init__(self,image,video,title):
+        self.image=image
+        self.video=video
+        self.title=title
+        
+    def plotChannel(self,img, channel, col, rows, plotNum, title):
+        cImg = img[:, :, channel]
+        plt.subplot(rows, col, plotNum)
+        plt.hist(np.ndarray.flatten(cImg), bins=256)
+        plt.title(title)    
+                
+    def show(self,waitTime):
+        hsv = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
+        plt.figure(figsize=(10, 8))
+        plt.suptitle('HSV Histogramm', fontsize=16)
+        self.plotChannel(hsv, 0, 1, 4, 2, 'Hue')
+        plt.subplots_adjust(hspace=.5)
+        self.plotChannel(hsv, 1, 1, 4, 3, 'Saturation')
+        self.plotChannel(hsv, 2, 1, 4, 4, 'Luminosity Value')
+        fig = plt.figure(1)
+        fig.canvas.set_window_title('Histogramm for %s' % (self.title))
+        plt.show(block=False)
+        if Histogram.debug:
+            self.video.showImage(self.image,self.title,keyCheck=True,keyWait=waitTime)
+        self.video.close()
+            
--- src/test_Histogram.py
+++ src/test_Histogram.py
@@ -5,19 +5,21 @@ from Histogram import Histogram
 from Environment import Environment
 from timeit import default_timer as timer
 from WebApp import WebApp,Warp
-from Video import Video
 from webchesscam import  WebChessCamArgs
+import time
 
 testEnv = Environment()
 
 
 def test_Histogram():
-    webApp = WebApp(WebChessCamArgs(["--debug"]).args)
+    webApp = WebApp(WebChessCamArgs([]).args)
     warpPointList=[
-        ([427,127],[ 962,180],[ 952,688],[430,691]),
-        ([285, 76],[1057, 93],[1050,870],[262,866])
+        ([427,180],[ 962,180],[ 952,688],[430,691]),
+        ([288, 76],[1057, 93],[1050,870],[262,866]),
+        ([132, 89],[ 492, 90],[ 497,451],[122,462]),
+        ([143, 18],[ 511, 13],[ 511,387],[147,386])
         ]
-    for index in range(1,3):
+    for index in range(1,5):
         start = timer()
         warpPoints=warpPointList[index-1]
         webApp.warp = Warp(list(warpPoints))
@@ -27,6 +29,10 @@ def test_Histogram():
         height, width = bgr.shape[:2]
         end = timer()
         print("%.3fs for loading image %d: %4d x %4d" % ((end-start),index,width,height))
+        title="image %d" % (index)
+        histogram=Histogram(bgr,video,title)
+        histogram.show(500)
+        
 
 test_Histogram()
     
