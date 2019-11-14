a4e7bc88fc8a214157fc21976e1ed87b7bac5428
Wolfgang Fahl <wf@bitplan.com>
2019-11-14 11:43:53 +0100

    test slow optimize on all test boards

--- src/test_IdealBoard.py
+++ src/test_IdealBoard.py
@@ -11,46 +11,57 @@ from timeit import default_timer as timer
 import cv2
 from PlotLib import PlotLib, PlotType
 
-
-def test_ChessBoard12():
-    plots=PlotLib("Ideal Colored Chessboard",PlotLib.A4(turned=True))
-    shortTime=500      
+debug=False
+shortTime=1000//20
+    
+def tryOptimization(chessboard,title):
+    if debug:
+        plots=PlotLib("Ideal Colored Chessboard for "+title,PlotLib.A4(turned=True))
+    
     #IdealBoard.debug=False
-    env=Environment()
-    chessboard12Path=env.testMedia+'chessBoard012.jpg'
-    print (chessboard12Path)
-    chessboard12=cv2.imread(chessboard12Path)
-    assert chessboard12 is not None
-    IdealBoard.showImage(chessboard12,title="original",waitTime=shortTime)
+    IdealBoard.showImage(chessboard,title="original "+title,waitTime=shortTime)
     start=timer()      
-    board=IdealBoard.createIdeal(527,527)
+    h, w = chessboard.shape[:2]
+    board=IdealBoard.createIdeal(w,h)
     end=timer()
     print('ideal board created in %.3fs' % (end-start))
     board.show(waitTime=shortTime)
     start=timer()  
-    diffImage=board.diff(chessboard12)
+    diffImage=board.diff(chessboard)
     end=timer()
     print('diff created in %.3fs' % (end-start))
     IdealBoard.showImage(diffImage,title="diff",waitTime=shortTime)   
     start=timer()
     minBoards=[]
-    optimized=IdealBoard.optimizeIdeal(chessboard12,minBoards)
+    optimized=IdealBoard.optimizeIdeal(chessboard,minBoards)
     end=timer()
     print('optimized in %.3fs' % (end-start))
     for minBoard in minBoards:
         rgb=cv2.cvtColor(minBoard.image,cv2.COLOR_BGR2RGB)
-        plots.addPlot(rgb, minBoard.debugInfo,minBoard.values,minBoard.diffSums)
+        if debug:
+            plots.addPlot(rgb, minBoard.debugInfo,minBoard.values,minBoard.diffSums)
         IdealBoard.showImage(minBoard.image, minBoard.debugInfo, waitTime=1)
     IdealBoard.showImage(image=optimized.image,title="optimized",waitTime=shortTime)
-    IdealBoard.showImage(image=optimized.diff(chessboard12),title="optimizedDiff",waitTime=shortTime)
-    board.save("/tmp/idealchessboard.jpg")
-    plots.createPDF('/tmp/idealcoloredboard',plotType=PlotType.PLOT,infos={'Title': 'Ideal colored board'})
+    IdealBoard.showImage(image=optimized.diff(chessboard),title="optimizedDiff",waitTime=shortTime)
+    board.save("/tmp/idealchessboard"+title+".jpg")
+    if debug:
+        plots.createPDF('/tmp/idealcoloredboard'+title,plotType=PlotType.PLOT,infos={'Title': 'Ideal colored board'})
+    
+def test_ChessBoard12():
+    env=Environment()
+    chessboard12Path=env.testMedia+'chessBoard012.jpg'
+    # print (chessboard12Path)
+    chessboard12=cv2.imread(chessboard12Path)
+    assert chessboard12 is not None
+    tryOptimization(chessboard12,"chessboard12")
+    
 
 def test_IdealBoard():
-    webApp = WebApp(WebChessCamArgs(["--debug","--speedup=4"]).args)
+    webApp = WebApp(WebChessCamArgs(["--speedup=4"]).args)
     for imageInfo in testEnv.imageInfos:
         bgr=testEnv.loadFromImageInfo(webApp,imageInfo)
+        tryOptimization(bgr, imageInfo['title'])
         
         
-test_ChessBoard12()
-#test_IdealBoard()
+#test_ChessBoard12()
+test_IdealBoard()
