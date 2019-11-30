55fa03b86887d15e88f6507b1cfab7640b377e62
Wolfgang Fahl <wf@bitplan.com>
2019-11-30 05:00:08 +0100

    speeds up BoardFinder by factor 25

--- pcwawc/BoardFinder.py
+++ pcwawc/BoardFinder.py
@@ -14,25 +14,48 @@ class BoardFinder(object):
     def __init__(self, image):
         self.image=image
         self.height, self.width = self.image.shape[:2]
-        if BoardFinder.debug:
-            print("BoardFinder for %dx%d image" % (self.width, self.height))
-
-    def find(self):
+       
+    def find(self,limit=1,searchWidth=640):
         startt=timer()
-        gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
-        found={}
-        for rows in range(3,9):
-            for cols in range(3,9):
+        sw=self.width
+        sh=self.height
+        if sw>searchWidth:
+            sw=searchWidth
+            sh=self.height*sw//self.width
+        self.searchimage=cv2.resize(self.image,(sw,sh))
+        if BoardFinder.debug:
+            print("BoardFinder for %dx%d image resized to %dx%d" % (self.width, self.height,sw,sh))
+
+        gray = cv2.cvtColor(self.searchimage, cv2.COLOR_BGR2GRAY)
+        fullSizeGray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        self.found={}
+        for rows in range(8,2,-1):
+            for cols in range(8,rows-1,-1):
                 chesspattern=(rows,cols)       
-                # Find the chessboard corners
-                start=timer()
-                ret, corners = cv2.findChessboardCorners(gray, chesspattern, None)
-                end=timer()
-                if ret == True:
-                    found[chesspattern]=corners
-                    if BoardFinder.debug:
-                        print ("finding corners with %d x %d pattern took %.2f s" % (rows,cols,(end-start)))
+                corners=self.findPattern(gray, chesspattern)
+                if corners is not None:
+                    fullSizeCorners=self.findPattern(fullSizeGray,chesspattern)
+                    if fullSizeCorners is not None:
+                        self.found[chesspattern]=fullSizeCorners
+                if len(self.found)>=limit:
+                    break
+            if len(self.found)>=limit:
+                    break                     
         endt=timer()
         if BoardFinder.debug:
-            print ("found %d patterns in %.1f s" % (len(found),(endt-startt)))
-        return found        
+            print ("found %d patterns in %.1f s" % (len(self.found),(endt-startt)))
+        return self.found
+
+    def findPattern(self,image,pattern):
+        """ try finding the chess board corners in the given image with the given pattern """
+        h,w = image.shape[:2]
+        start=timer()
+        ret, corners = cv2.findChessboardCorners(image, pattern, None)
+        end=timer()
+        if BoardFinder.debug:
+            rows,cols=pattern
+            print ("%dx%d in %dx%d after %.3f s: %s" % (rows,cols,w,h,(end-start),"✔" if ret else "❌"))
+        if ret == True:
+            return corners
+        else:
+            return None
--- tests/test_BoardFinder.py
+++ tests/test_BoardFinder.py
@@ -2,17 +2,21 @@
 # part of https://github.com/WolfgangFahl/play-chess-with-a-webcam
 from pcwawc.boardfinder import BoardFinder
 from pcwawc.Environment4Test import Environment4Test
+from timeit import default_timer as timer
 
 testEnv = Environment4Test()
 
 # test finding a chess board
 def test_findBoard():
     BoardFinder.debug=True
+    startt=timer()
     for imageInfo in testEnv.imageInfos:
         fen=imageInfo["fen"]
         image,video,warp=testEnv.prepareFromImageInfo(imageInfo)      
         finder = BoardFinder(image)
-        found=finder.find()
+        found=finder.find(limit=1,searchWidth=360)
         assert(len(found)>0)
-
+    endt=timer()
+    if BoardFinder.debug:
+            print ("found %d chessboards in %.1f s" % (len(testEnv.imageInfos),(endt-startt)))
 test_findBoard()
