1c8215d7f85a8e936b9764839853fa86c21741c7
Wolfgang Fahl <wf@bitplan.com>
2019-12-10 09:41:02 +0100

    fixes imports in test_BoardFinder

--- tests/test_BoardFinder.py
+++ tests/test_BoardFinder.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 # part of https://github.com/WolfgangFahl/play-chess-with-a-webcam
 from pcwawc.boardfinder import BoardFinder,Corners
-from pcwawc.Environment4Test import Environment4Test
-from pcwawc.Environment import Environment
-from pcwawc.ChessTrapezoid import Trapez2Square
-from pcwawc.Video import Video
+from pcwawc.environment4test import Environment4Test
+from pcwawc.environment import Environment
+from pcwawc.chesstrapezoid import Trapez2Square
+from pcwawc.video import Video
 from timeit import default_timer as timer
 import chess
 testEnv = Environment4Test()
@@ -18,46 +18,46 @@ def test_findBoard():
     for imageInfo in testEnv.imageInfos:
         title=imageInfo["title"]
         fen=imageInfo["fen"]
-        image,video,warp=testEnv.prepareFromImageInfo(imageInfo)      
+        image,video,warp=testEnv.prepareFromImageInfo(imageInfo)
         finder = BoardFinder(image,video=video)
         corners=finder.findOuterCorners(searchWidth=360)
         if fen==chess.STARTING_BOARD_FEN:
             assert corners.rows<=corners.cols
         else:
-            assert corners.rows==corners.cols    
-        
+            assert corners.rows==corners.cols
+
         if BoardFinder.debug:
             corners.showDebug(image,title)
-            
+
         histograms=finder.getHistograms(image, title, corners)
-        
+
         if BoardFinder.debug:
             # wait for background histogram to be available -- see a few lines below
             #finder.showHistogramDebug(histograms,title,corners)
             finder.showPolygonDebug(image,title,corners)
-            
-        finder.expand(image,title,histograms,corners)     
-                  
+
+        finder.expand(image,title,histograms,corners)
+
         if BoardFinder.debug:
             finder.showHistogramDebug(histograms,title,corners)
         endt=timer()
     if BoardFinder.debug:
         print ("found %d chessboards in %.1f s" % (len(testEnv.imageInfos),(endt-startt)))
-    
+
 def test_SortPoints():
     points=[(0,0),(0,1),(1,1),(1,0)]
     center=BoardFinder.centerXY(points)
     assert center==(0.5,0.5)
     sortedPoints=BoardFinder.sortPoints(points)
     assert sortedPoints==[(0, 0), (1, 0), (1, 1), (0, 1)]
-    
+
 def test_Trapez2Square():
     t2s=Trapez2Square((100,100),(200,100),(100,0),(0,0))
     assert t2s.relativeToTrapezXY(0,0)==(100.0,100.0)
     assert t2s.relativeToTrapezXY(1,0)==(200.0,100.0)
     assert t2s.relativeToTrapezXY(1,1)==(100.0,  0.0)
     assert t2s.relativeToTrapezXY(0,1)==(  0.0,  0.0)
-    
+
 def test_MovingBoard():
     video=Video()
     testvideos=['emptyBoard001','baxter']
@@ -76,7 +76,7 @@ def test_MovingBoard():
             if quitWanted:
                 break
             if ret:
-                try:        
+                try:
                     title="corners-%s-%04d" % (testvideo,frames)
                     startt=timer()
                     finder = BoardFinder(image,video=video)
@@ -92,11 +92,11 @@ def test_MovingBoard():
                 except Exception as ex:
                     print ("%3d/%3d: %s" % (found,frames,ex))
             if frames%speedup==0:
-                video.showImage(image,"original")        
-        video.close()  
+                video.showImage(image,"original")
+        video.close()
         assert found==expected[testindex]
-                
-test_SortPoints()        
+
+test_SortPoints()
 test_Trapez2Square()
 test_findBoard()
 test_MovingBoard()
