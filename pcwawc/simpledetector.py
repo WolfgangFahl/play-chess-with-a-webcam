6436598591077a465afc3a52e90968e173b19061
Wolfgang Fahl <wf@bitplan.com>
2019-12-16 08:59:56 +0100

    refactors simple8x8 detector

--- pcwawc/simpledetector.py
+++ pcwawc/simpledetector.py
@@ -10,6 +10,43 @@ import cv2
 from zope.interface import implementer
 from timeit import default_timer as timer
 
+class ImageChange:
+    """ detect change of a single image """
+    
+    thresh=150
+    averageWindow=4
+    def __init__(self):
+        self.cbPreviousBW=None
+        self.movingAverage=MovingAverage(ImageChange.averageWindow)
+
+    def check(self,cbImage):
+        self.makeGray(cbImage)
+        self.calcPixelChanges()
+    
+    def makeGray(self,cbImage):
+        self.cbImage=cbImage
+        image=cbImage.image
+        imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        self.cbImageGray=ChessBoardImage(cv2.cvtColor(imageGray,cv2.COLOR_GRAY2BGR),"gray")
+        # @TODO Make the treshold 150 configurable
+        thresh=ImageChange.thresh
+        (thresh, self.imageBW) = cv2.threshold(imageGray, thresh, 255, cv2.THRESH_TRUNC)
+        self.cbImageBW=ChessBoardImage(cv2.cvtColor(self.imageBW,cv2.COLOR_GRAY2BGR),"bw")
+        self.updatePrevious(self.cbImageBW)
+        self.cbDiffImage=self.cbImageBW.diffBoardImage(self.cbPreviousBW)
+        
+    def updatePrevious(self,cbImageBW):    
+        if self.cbPreviousBW is None:
+            self.cbPreviousBW=cbImageBW
+        
+    def calcPixelChanges(self):
+        self.pixelChanges=cv2.norm(self.cbImageBW.image, self.cbPreviousBW.image, cv2.NORM_L1) / self.cbImageBW.pixels
+        self.movingAverage.push(self.pixelChanges)
+        
+    def __str__(self):    
+        text="change: %4.1f, average: %4.1f, pixels: %d" % (self.pixelChanges,self.movingAverage.mean(),self.cbImageBW.pixels)
+        return text
+    
 @implementer(IMoveDetector) 
 class SimpleDetector:
     """ a simple treshold detector """
@@ -28,59 +65,39 @@ class SimpleDetector:
         if vision.warp.warping:
             cbWarped=cbImageSet.cbWarped
             start=timer()
-            cbWarpedGray,cbWarpedBW,cbDiffImage,pixelChanges=self.imageChange.check(cbWarped)
-            cbImageSet.cbDebug=cbImageSet.debugImage2x2(cbWarped,cbWarpedGray,cbWarpedBW,cbDiffImage)
+            self.imageChange.check(cbWarped)
+            ic=self.imageChange
+            cbImageSet.cbDebug=cbImageSet.debugImage2x2(cbWarped,ic.cbImageGray,ic.cbImageBW,ic.cbDiffImage)
             endt=timer()    
-            print ('Frame: %5d %.3f s, change: %4.1f, average: %4.1f, pixels: %d' % (cbImageSet.frameIndex,endt-start,pixelChanges,self.imageChange.movingAverage.mean(),cbWarped.pixels))  
+            print ('Frame %5d %.3f s:%s' % (cbImageSet.frameIndex,endt-start,ic))  
 
-class ImageChange:
-    """ change of a single image """
-    def __init__(self):
-        self.cbPreviousBW=None
-        # @TODO make lenght of moving average configurable
-        self.movingAverage=MovingAverage(4)
-    
-    def check(self,cbImage):
-        image=cbImage.image
-        imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        cbImageGray=ChessBoardImage(cv2.cvtColor(imageGray,cv2.COLOR_GRAY2BGR),"gray")
-        # @TODO Make the treshold 150 configurable
-        thresh=150
-        (thresh, imageBW) = cv2.threshold(imageGray, thresh, 255, cv2.THRESH_TRUNC)
-        cbImageBW=ChessBoardImage(cv2.cvtColor(imageBW,cv2.COLOR_GRAY2BGR),"bw")
-        if self.cbPreviousBW is None:
-            self.previousBW=imageBW
-            self.cbPreviousBW=cbImageBW
-        cbDiffImage=cbImageBW.diffBoardImage(self.cbPreviousBW)
-        self.pixelChanges=cv2.norm(imageBW, self.previousBW, cv2.NORM_L1) / cbImage.pixels
-        self.movingAverage.push(self.pixelChanges)
-        return cbImageGray,cbImageBW,cbDiffImage,self.pixelChanges
-    
 @implementer(IMoveDetector) 
-class Simple8x8Detector:
+class Simple8x8Detector(SimpleDetector):
     """ a simple treshold per field detector  """
     # construct me 
     def __init__(self):
-        pass
+        super().__init__()
     
     def setup(self,name,vision):
-        self.name=name
-        self.vision=vision
-        self.board=vision.board
+        super().setup(name,vision)
+        self.board=self.vision.board
         self.imageChanges={}
         for square in self.board.genSquares():
             self.imageChanges[square.an]=ImageChange()
         
     def onChessBoardImage(self,imageEvent):
+        super().onChessBoardImage(imageEvent)
         cbImageSet=imageEvent.cbImageSet
         vision=cbImageSet.vision
         if vision.warp.warping:
             cbWarped=cbImageSet.cbWarped
             # TODO only do once ...
-            startt=timer()
             self.board.divideInSquares(cbWarped.width,cbWarped.height)
+            # calculate pixelChanges per square based on parts of the bigger images created by the super class
             for square in self.board.genSquares():
-                squareImage=ChessBoardImage(square.getSquareImage(cbWarped),square.an)
-                self.imageChanges[square.an].check(squareImage)
-            endt=timer()  
-            print ("%4d: %.3fs" % (cbImageSet.frameIndex,endt-startt))
+                ic=self.imageChanges[square.an]
+                ic.cbImageBW=ChessBoardImage(square.getSquareImage(self.imageChange.cbImageBW),square.an)
+                ic.updatePrevious(ic.cbImageBW)
+                ic.calcPixelChanges()
+                
+                print ("%4d %s: %s" % (cbImageSet.frameIndex,square.an,ic))
