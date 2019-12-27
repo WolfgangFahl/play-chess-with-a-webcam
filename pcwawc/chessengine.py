d37fc9e60d5476f40a3b03a7f303fdef96e2cb3e
Wolfgang Fahl <wf@bitplan.com>
2019-12-27 09:24:42 +0100

    improves chessengine test

--- pcwawc/chessengine.py
+++ pcwawc/chessengine.py
@@ -5,7 +5,6 @@ Created on 2019-12-16
 '''
 # part of https://github.com/WolfgangFahl/play-chess-with-a-webcam
 import chess.engine
-import asyncio
 import shutil
 
 class Engine:
@@ -17,30 +16,47 @@ class Engine:
     # list of engines to search for
     engineConfigs=[
         {
-            "name": "GNU Chess",
-            "command":"gnuchess",
-            "url":"https://www.gnu.org/software/chess/"
-        },
-        {
             "name": "Crafty Computer Chess",
             "command":"crafty",
-            "url": "http://www.craftychess.com/"
+            "url": "http://www.craftychess.com/",
         },
         {
-            "name:": "Stockfish Chess",
+            "name": "GNU Chess",
+            "command":"gnuchess",
+            "url":"https://www.gnu.org/software/chess/",
+        },
+        {
+            "name": "Stockfish Chess",
             "command":"stockfish",
             "url":"https://stockfishchess.org/"
-        }]
+        },
+        {
+            "name": "XBoard",
+            "command":"xboard",
+            "url":"https://www.gnu.org/software/xboard/manual/xboard.html"
+        }  
+        ]
     
-    def __init__(self,engineConfig):
+    def __init__(self,engineConfig,timeout=5):
         self.engineCmd=engineConfig["command"]
         self.enginePath=engineConfig["path"]
         self.name=engineConfig["name"]
         self.url=engineConfig["url"]
-        
-    def start(self):
+        if "tested" in engineConfig:
+            self.tested=engineConfig["tested"]
+        self.timeout=timeout
+        #asyncio.set_event_loop_policy(chess.engine.EventLoopPolicy())       
         # https://python-chess.readthedocs.io/en/latest/engine.html
-        pass    
+        self.error=None
+    
+    def open(self):
+        try:
+            self.engine = chess.engine.SimpleEngine.popen_uci(self.engineCmd,timeout=self.timeout,debug=Engine.debug)
+        except Exception as te:
+            self.engine=None
+            self.error=te
+            pass
+        return self.engine
         
     def __str__(self):
         text="chess engine %s called via %s at %s" % (self.name,self.engineCmd,self.enginePath)
--- tests/test_chessengine.py
+++ tests/test_chessengine.py
@@ -4,29 +4,71 @@ Created on 2019-12-26
 @author: wf
 '''
 from pcwawc.chessengine import Engine
+import chess
+import chess.pgn
+from datetime import datetime
 import os
 
 debug=True
 
 def fix_path():
     """ fixes PATH issues e.g. on MacOS macports environment - adopt to your needs as you see fit"""
-    
-    # is macports installed?
-    if os.path.isfile("/opt/local/bin/port"):
+    home=os.environ["HOME"]
+    homebin=home+"/bin"
+    # is a home/bin directory available?
+    if os.path.isdir(homebin):
         if debug:
-            print ("found macports - adding /opt/local/bin to PATH")
-        os.environ["PATH"]=os.environ["PATH"]+":"+"/opt/local/bin"
-    if debug:
-        print ("PATH is: %s" % (os.environ['PATH']))
+            print ("found %s - adding it to PATH" % homebin)
+        os.environ["PATH"]="%s:%s" % (os.environ["PATH"],homebin)
         
-# http://julien.marcel.free.fr/macchess/Chess_on_Mac/Engines.html
-def test_engines():
+def findEngines():
     fix_path()
     Engine.debug=debug
     engineConfigs=Engine.findEngines()
+    return engineConfigs
+        
+# http://julien.marcel.free.fr/macchess/Chess_on_Mac/Engines.html
+def test_engines():
+    engineConfigs=Engine.findEngines()
     for key,engineConfig in engineConfigs.items():
-        engine=Engine(engineConfig)
+        engine=Engine(engineConfig,timeout=1.5)
         print (engine)
-            
+        engine.open()
+        
+def test_play():
+    engineConfigs=findEngines() 
+    engineCmd="stockfish"
+    if engineCmd not in engineConfigs:
+        raise Exception("%s engine not found" % engineCmd)
+    chessEngine=Engine(engineConfigs[engineCmd])
+    engine=chessEngine.open()
+    if engine is None:
+        raise Exception("Could not open %s engine" % engineCmd)
+    board = chess.Board()
+    game = chess.pgn.Game() 
+    game.headers["Event"] = "test_chessengine %s" % (chessEngine.name)
+    game.headers["Site"] = "http://wiki.bitplan.com/index.php/PlayChessWithAWebCam" 
+    game.headers["White"] = "%s" % (chessEngine.name)
+    game.headers["Black"] = "%s" % (chessEngine.name)
+    game.headers["Date"] = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+    moveIndex=0
+    node=None
+    while not board.is_game_over():
+        result = engine.play(board, chess.engine.Limit(time=0.1))
+        move=str(result.move)
+        board.push(result.move)
+        print ("%d-%s: %s" % (moveIndex//2+1,"white" if moveIndex%2==0 else "black",move))
+        print (board.unicode())
+        if moveIndex == 0:
+            node = game.add_variation(chess.Move.from_uci(move))
+        else:
+            node = node.add_variation(chess.Move.from_uci(move))
+        moveIndex+=1
 
-test_engines()
+    engine.quit()    
+    print (game)   
+    # try pasting resulting pgn to
+    # https://lichess.org/paste
+            
+test_play()
+#test_engines()
