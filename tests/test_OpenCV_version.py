33a953b2bb22e1ea2bbba79eeb3cd6267d9d8105
Wolfgang Fahl <wf@bitplan.com>
2021-06-09 10:01:33 +0200

    bumps opencv version for #32

--- tests/test_OpenCV_version.py
+++ tests/test_OpenCV_version.py
@@ -4,8 +4,11 @@ import cv2
 from unittest import TestCase
 
 class OpenCV_VersionTest(TestCase):
+    '''
+    test the OpenCV Version being used
+    '''
     def test_Version(self):
         version = cv2.__version__
         print (version)
         # compare with requirements.txt
-        assert version == "4.1.2"
+        assert version == "4.3.0"
